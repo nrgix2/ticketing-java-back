82c8a359bbb72ec4d7fa695695311e84d1fdafaf
Antoine Mousset <antoine.mousset@ynov.com>
2022-01-05 10:04:56 +0100

    feat: add user for a ticket

--- src/main/java/com/j2ee/ticketing/entities/Tickets.java
+++ src/main/java/com/j2ee/ticketing/entities/Tickets.java
@@ -1,10 +1,13 @@
 package com.j2ee.ticketing.entities;
 
 import javax.persistence.Column;
+import com.j2ee.ticketing.entities.Users;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.Valid;
 
@@ -30,11 +33,33 @@ public class Tickets {
 	 	@Valid
 	    @Column(name="descritpion", length=255, nullable=false)
 	    private String descritpion;
+	 	
+	 	 @ManyToOne()
+	     @JoinColumn(name = "user_id")
+	     private Users user;
+
 
 	 	
-	 	public String getName() {
-			return name;
+	 	public String getDescritpion() {
+			return descritpion;
 		}
+
+		public void setDescritpion(String descritpion) {
+			this.descritpion = descritpion;
+		}
+
+
+		public Users getUser() {
+			return user;
+		}
+
+		public void setUser(Users user) {
+			this.user = user;
+		}
+
+		public String getName() {
+			return name;
+		} 
 
 		public void setName(String name) {
 			this.name = name;
